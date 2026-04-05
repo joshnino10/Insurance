b6edd0c255d905e8fb4a388cfbde77a8d49cacc1
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-04-05 18:38:36 +0100

    j

--- app/(tab)/chat.tsx
+++ app/(tab)/chat.tsx
@@ -1,6 +1,6 @@
 import Chatflow from '@/Component/BotScreen/Chatflow'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 
 export default function Chat() {
