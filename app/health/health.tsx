60136914fa8db481256da992e06696ec4abda41c
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-04-09 05:46:00 +0100

    j

--- app/health/health.tsx
+++ app/health/health.tsx
@@ -1,32 +1,31 @@
-import { StyleSheet,Platform, ScrollView } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import HealthWallet from '@/Component/HealthScreen.tsx/HealthWallet'
-import HealthDescription from '@/Component/HealthScreen.tsx/HealthDescription'
-import RecentTransaction from '@/Component/HealthScreen.tsx/RecentTransaction'
-import MonthlySummery from '@/Component/HealthScreen.tsx/MonthlySummery'
+import { StyleSheet, Platform, ScrollView } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import HealthWallet from "@/Component/HealthScreen.tsx/HealthWallet";
+import HealthDescription from "@/Component/HealthScreen.tsx/HealthDescription";
+import RecentTransaction from "@/Component/HealthScreen.tsx/RecentTransaction";
+import MonthlySummery from "@/Component/HealthScreen.tsx/MonthlySummery";
 
 export default function Health() {
   return (
     <SafeAreaView style={styles.SafeArea}>
-        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:60}}>
-
-        <HealthWallet/>
-        <HealthDescription/>
-        <RecentTransaction/>
-        <MonthlySummery/>
-        </ScrollView>
-     
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
+        <HealthWallet />
+        <HealthDescription />
+        <RecentTransaction />
+        <MonthlySummery />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    SafeArea:{
-          flex:1,
-          backgroundColor:'#D9D9D900',
-          paddingTop: Platform.OS === 'android'? 10:0
-
-    }
-
-})
+  SafeArea: {
+    flex: 1,
+    backgroundColor: "#D9D9D900",
+    paddingTop: Platform.OS === "android" ? 10 : 0,
+  },
+});
